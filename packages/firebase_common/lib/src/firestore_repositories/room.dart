0b41b5afbd1b5bdba92c156cb7d4b513e31bab93
naokiwakata <rockwakata15@gmail.com>
2023-09-30 15:23:02 +0900

    RoomRepositoryのメソッドを追加

--- packages/firebase_common/lib/src/firestore_repositories/room.dart
+++ packages/firebase_common/lib/src/firestore_repositories/room.dart
@@ -2,4 +2,25 @@ import '../firestore_documents/_export.dart';
 
 class RoomRepository {
   final _query = RoomQuery();
+
+  Stream<ReadRoom?> subscribeRoom({required String roomId}) =>
+      _query.subscribeDocument(roomId: roomId);
+
+  Future<void> createRoom({required String spotDifferenceId}) async {
+    await _query.add(
+      createRoom: CreateRoom(
+        spotDifferenceId: spotDifferenceId,
+        roomStatus: RoomStatus.pending,
+      ),
+    );
+  }
+
+  Future<void> completeRoom({required String roomId}) async {
+    await _query.update(
+      roomId: roomId,
+      updateRoom: const UpdateRoom(
+        roomStatus: RoomStatus.completed,
+      ),
+    );
+  }
 }
