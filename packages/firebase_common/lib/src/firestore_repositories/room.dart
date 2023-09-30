cc7f98c20416ab647ebd8884ad395e1deac84a67
naokiwakata <rockwakata15@gmail.com>
2023-09-30 15:26:31 +0900

    Doc commentを追加

--- packages/firebase_common/lib/src/firestore_repositories/room.dart
+++ packages/firebase_common/lib/src/firestore_repositories/room.dart
@@ -3,9 +3,11 @@ import '../firestore_documents/_export.dart';
 class RoomRepository {
   final _query = RoomQuery();
 
+  /// 指定した [roomId] に一致する [Room] を購読する
   Stream<ReadRoom?> subscribeRoom({required String roomId}) =>
       _query.subscribeDocument(roomId: roomId);
 
+  /// 指定した [spotDifferenceId] を持つ [Room] を作成する
   Future<void> createRoom({required String spotDifferenceId}) async {
     await _query.add(
       createRoom: CreateRoom(
@@ -15,6 +17,7 @@ class RoomRepository {
     );
   }
 
+  /// 指定した [roomId] に一致する [Room] の roomStatus を completed に変更する
   Future<void> completeRoom({required String roomId}) async {
     await _query.update(
       roomId: roomId,
