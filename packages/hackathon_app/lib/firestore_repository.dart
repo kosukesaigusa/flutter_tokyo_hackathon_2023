711fbb9106586c548fa3ff39941841131a0be7f0
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-09-30 16:07:53 +0900

    add: RepositoryProvider の作成

--- packages/hackathon_app/lib/firestore_repository.dart
+++ packages/hackathon_app/lib/firestore_repository.dart
@@ -5,5 +5,28 @@ final appUserRepositoryProvider = Provider.autoDispose<AppUserRepository>(
   (_) => AppUserRepository(),
 );
 
-final todoRepositoryProvider =
-    Provider.autoDispose<TodoRepository>((_) => TodoRepository());
+final todoRepositoryProvider = Provider.autoDispose<TodoRepository>(
+  (_) => TodoRepository(),
+);
+
+final roomRepositoryProvider = Provider.autoDispose<RoomRepository>(
+  (_) => RoomRepository(),
+);
+
+final spotDifferenceRepositoryProvider =
+    Provider.autoDispose<SpotDifferenceRepository>(
+  (_) => SpotDifferenceRepository(),
+);
+
+final pointRepositoryProvider = Provider.autoDispose<PointRepository>(
+  (_) => PointRepository(),
+);
+
+final answerRepositoryProvider = Provider.autoDispose<AnswerRepository>(
+  (_) => AnswerRepository(),
+);
+
+final completedUserRepositoryProvider =
+    Provider.autoDispose<CompletedUserRepository>(
+  (_) => CompletedUserRepository(),
+);
