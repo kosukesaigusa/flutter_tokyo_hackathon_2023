614e15f4b6d8a38f7c9ef5f6354dda871ecf4b1b
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-10-01 03:55:50 +0900

    chore: remove unnecessary dependencies

--- packages/hackathon_app/lib/root/ui/root.dart
+++ packages/hackathon_app/lib/root/ui/root.dart
@@ -1,4 +1,3 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,15 +9,9 @@ import '../../spot_difference/ui/start_spot_difference.dart';
 final rootPageKey = Provider((ref) => GlobalKey<NavigatorState>());
 
 /// メインの [BottomNavigationBar] を含む画面。
-@RoutePage()
+
 class RootPage extends HookConsumerWidget {
   const RootPage({super.key});
-
-  /// [AutoRoute] で指定するパス文字列。
-  static const path = '/';
-
-  /// [RootPage] に遷移する際に `context.router.pushNamed` で指定する文字列。
-  static const location = path;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
