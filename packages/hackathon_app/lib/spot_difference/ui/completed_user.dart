097fa2e48650196e70c68038291233ff3317b1e8
naokiwakata <rockwakata15@gmail.com>
2023-09-30 21:26:52 +0900

    RankingCardウィジェットを作成

--- packages/hackathon_app/lib/spot_difference/ui/completed_user.dart
+++ packages/hackathon_app/lib/spot_difference/ui/completed_user.dart
@@ -26,35 +26,12 @@ class CompletedUserScreen extends ConsumerWidget {
                 final appUser = appUsers[index];
                 return Padding(
                   padding: const EdgeInsets.all(8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text((index + 1).toString()),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      if (appUser?.imageUrl != null)
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(appUser!.imageUrl),
-                          radius: 30,
-                        )
-                      else
-                        CircleAvatar(
-                          backgroundColor: Colors.grey[200],
-                          radius: 30,
-                          child: const Icon(
-                            Icons.person,
-                          ),
-                        ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: Text(
-                          appUser?.displayName ?? '',
-                        ),
-                      ),
-                    ],
+                  child: Center(
+                    child: _RankingCard(
+                      imageUrl: appUser?.imageUrl,
+                      displayName: appUser?.displayName,
+                      index: index,
+                    ),
                   ),
                 );
               },
@@ -64,6 +41,56 @@ class CompletedUserScreen extends ConsumerWidget {
       },
       error: (e, st) => Text(e.toString()),
       loading: () => const CircularProgressIndicator(),
+    );
+  }
+}
+
+class _RankingCard extends StatelessWidget {
+  const _RankingCard({
+    required this.imageUrl,
+    required this.displayName,
+    required this.index,
+  });
+
+  final int index;
+  final String? imageUrl;
+  final String? displayName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: 300,
+      child: Row(
+        children: [
+          Text((index + 1).toString()),
+          const SizedBox(
+            width: 10,
+          ),
+          if (imageUrl != null)
+            CircleAvatar(
+              backgroundImage: NetworkImage(imageUrl!),
+              radius: 30,
+            )
+          else
+            CircleAvatar(
+              backgroundColor: Colors.grey[200],
+              radius: 30,
+              child: const Icon(
+                Icons.person,
+              ),
+            ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(
+            displayName ?? '',
+          ),
+        ],
+      ),
     );
   }
 }
